4a25422bb34704bfc3db3873dcbbbcbb6a7f4e2a
Sangeetha R <sangeetha.raveendrannair@tryzens.com>
2023-03-03 18:18:33 +0530

    GROPT-54 Replicate Cart Functionality

--- extension/src/api/replicateCart/replicateCart.controller.ts
+++ extension/src/api/replicateCart/replicateCart.controller.ts
@@ -1,69 +1,96 @@
-import { ServerResponse } from "http"
+import {ServerResponse} from "http"
 
-import { StatusCodes, getReasonPhrase } from "http-status-codes"
+import {StatusCodes, getReasonPhrase} from "http-status-codes"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { getTransactionById, getOrder, updateStatus, Constants } from "@gr4vy-ct/common"
+import {getOrder, updateStatus, replicateCartFromOrder, Constants} from "@gr4vy-ct/common"
 
-import { Request } from "./../../types"
+import {Request} from "./../../types"
 import ResponseHelper from "./../../helper/response"
-import { isPostRequest } from "./../../helper/methods"
-import { getLogger } from "./../../utils"
+import {isPostRequest} from "./../../helper/methods"
+import {getLogger} from "./../../utils"
 
 const logger = getLogger()
 
 const processRequest = async (request: Request, response: ServerResponse) => {
-  try {
+    try {
+        if (!isPostRequest(request)) {
+            logger.debug(`Received non-POST request: ${request.method}. The request will not be processed!`)
+            return ResponseHelper.setResponseError(response, {
+                httpStatusCode: StatusCodes.BAD_REQUEST,
+                errors: [
+                    {
+                        code: StatusCodes.BAD_REQUEST,
+                        message: getReasonPhrase(StatusCodes.BAD_REQUEST),
+                    },
+                ],
+            })
+        }
+        const {orderId} = request.body
+        if (!orderId) {
+            throw {
+                message: `Required parameter orderId is missing or empty`,
+                statusCode: 400,
+            }
+        }
 
-   console.log('11111')
-      if (!isPostRequest(request)) {
-          logger.debug(`Received non-POST request: ${request.method}. The request will not be processed!`)
-          return ResponseHelper.setResponseError(response, {
-              httpStatusCode: StatusCodes.BAD_REQUEST,
-              errors: [
-                  {
-                      code: StatusCodes.BAD_REQUEST,
-                      message: getReasonPhrase(StatusCodes.BAD_REQUEST),
-                  },
-              ],
-          })
-      }
-      const { gr4vyTransactionId } = request.body
+        const order = await getOrder({ request, orderId })
+        if (!order) {
+            throw {
+                message: `Error in fetching CT order`,
+                statusCode: 400,
+            }
+        }
 
-      if (!gr4vyTransactionId) {
-          throw {
-              message: `Required parameter gr4vyTransactionId is missing or empty`,
-              statusCode: 400,
-          }
-      }
-    const orderId = '92e1ab16-bb1c-438f-be5f-c954041a4520'
-      // Fetch order id from the transaction
-      /*const {
-          external_identifier: orderId,
-          status,
-          amount: gr4vyTransactionAmount,
-      } = gr4vyTransaction?.body || {}*/
-    // // Get order payment and transaction details
-    const {order} = await getOrder({orderId: orderId})
-    console.log('333')
-    ResponseHelper.setResponseTo200(response, {})
+        const version  = order?.version || {}
+        if (!version) {
+            throw {
+                message: `Error in fetching version for order ID ${orderId}`,
+                statusCode: 400,
+            }
+        }
 
-  } catch (e) {
-    const errorStackTrace =
-      `Error during parsing replicate cart request: Ending the process. ` +
-      `Error: ${JSON.stringify(e)}`
-    logger.error(errorStackTrace)
+        let orderState, orderPaymentState, transactionState
+        const {
+            STATES: { CT },
+        } = Constants
 
-    ResponseHelper.setResponseError(response, {
-      httpStatusCode: e.statusCode || 500,
-      errors: [
-        {
-          code: e.statusCode || 500,
-          message: e?.response?.body?.message || e.message,
-        },
-      ],
-    })
-  }
+        orderState = CT.ORDER.CANCELLED
+        orderPaymentState = CT.ORDERPAYMENT.FAILED
+        transactionState = CT.TRANSACTION.FAILURE
+
+        //Cancel order
+        await updateStatus({
+            order,
+            orderState,
+            orderPaymentState,
+            transactionState,
+        })
+
+        const replicateCartResult = await replicateCartFromOrder({orderId })
+
+        const replicateCartResponseData = {
+            cartId: replicateCartResult.id,
+        }
+
+        ResponseHelper.setResponseTo200(response, replicateCartResponseData)
+
+    } catch (e) {
+        const errorStackTrace =
+            `Error during parsing replicate cart request: Ending the process. ` +
+            `Error: ${JSON.stringify(e)}`
+        logger.error(errorStackTrace)
+
+        ResponseHelper.setResponseError(response, {
+            httpStatusCode: e.statusCode || 500,
+            errors: [
+                {
+                    code: e.statusCode || 500,
+                    message: e?.response?.body?.message || e.message,
+                },
+            ],
+        })
+    }
 }
 
-export default { processRequest }
+export default {processRequest}
---
+++ gr4vy-ct-common/src/services/commercetools/replicateCartFromOrder/query.ts
@@ -0,0 +1,14 @@
+// GraphQL query to replicate cart from order
+const replicateCartFromOrderQuery = `
+mutation replicateCartFromOrder($orderId: String!){
+  replicateCart(    
+    reference:{
+      typeId:"order"
+      id:$orderId
+    }
+  ){
+    id
+  }
+}
+`
+export {replicateCartFromOrderQuery}
