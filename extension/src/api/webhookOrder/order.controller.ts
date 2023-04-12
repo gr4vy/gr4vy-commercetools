1fe470cdbefafba64c376012bab0fa164aadd83e
Manuraj M <manuraj.m@tryzens.com>
2023-04-12 11:54:35 +0530

    Fixed webhook event type issue

--- extension/src/api/webhookOrder/order.controller.ts
+++ extension/src/api/webhookOrder/order.controller.ts
@@ -44,14 +44,7 @@ const processRequest = async (request: Request, response: ServerResponse) => {
       }
     }
 
-    if (!target || (target.type && target.type !== "transaction")) {
-      throw {
-        message: `A Webhook request was received with an invalid entity target type - ${target?.type}`,
-        statusCode: 422,
-      }
-    }
-
-    const gr4vyTransactionId = target?.id
+    const gr4vyTransactionId = target?.transaction_id
 
     if (!gr4vyTransactionId) {
       throw {
